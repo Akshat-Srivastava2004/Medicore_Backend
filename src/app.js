37d309a766ff7cbb4dd6ece29b5e6b83deb78fdf
Akshat-Srivastava2004 <akshatsri2004alld@gmail.com>
2025-01-20 20:59:09 +0530

    updateroutess

--- src/app.js
+++ src/app.js
@@ -46,17 +46,11 @@ app.use(express.urlencoded({
 app.use(cookieParser())             
 
 // app.use("/api/v1/users", userRouter);
-app.use(
-    cors({
-      origin: [
-        "https://medicalrecommendation.netlify.app/",
-         "http://localhost:3000" // Allow localhost for development 
-      ],
-      methods: ["GET", "POST", "PUT", "DELETE"], // Specify the HTTP methods you allow
-      credentials: true, // Enable this if you use cookies or HTTP authentication
-    })
-  );
-  
+app.use(cors({
+    origin: ['https://medicalrecommendation.netlify.app', 'http://localhost:3000'], // Allow these origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+}));
   // Routes
   app.use("/api/v1/users", userRouter);
   
